8084d6e3c0373e2c43cdd0d3db81b3a4cbd1fd33
Chris <cbv133@gmail.com>
2019-06-29 08:04:03 +0300

    Added quantity input to product details view.

--- src/EBuy.Web/wwwroot/js/site.js
+++ src/EBuy.Web/wwwroot/js/site.js
@@ -1,4 +1,17 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+let quantity = document.getElementById('quantity');
+
+document.querySelector('#quantity-increment').addEventListener('click', incrementQuantity);
+document.querySelector('#quantity-decrement').addEventListener('click', decrementQuantity);
+
+function incrementQuantity(e) {
+    e.preventDefault();
+    quantity.value = +quantity.value + 1;
+}
+
+function decrementQuantity(e) {
+    e.preventDefault();
+    quantity.value = +quantity.value - 1;
+}
