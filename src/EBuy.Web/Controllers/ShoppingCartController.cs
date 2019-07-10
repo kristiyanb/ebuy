bf215dadcedc3d77dd09439fbe2c0754808b6a51
Chris <cbv133@gmail.com>
2019-07-11 01:12:27 +0300

    Added shopping cart controller and index view.

---
+++ src/EBuy.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,13 @@
+﻿namespace EBuy.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    public class ShoppingCartController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
